3ae0d73b840010414a40c0e7a1cc1fa59c30c013
jspilinek <101371702+jspilinek@users.noreply.github.com>
2022-12-15 11:50:45 -0600

    Corrected comments

--- sql/SharedPoolAdvice.sql
+++ sql/SharedPoolAdvice.sql
@@ -9,8 +9,8 @@ DEF prevpage = 'PgaTargetAdvice.html';
 SET MARK HTML OFF;
 PRO <h2>Comments</h2>
 PRO <p>Predicts how adjusting the shared pool will affect parse time.</p>
-PRO <p><b>SIZE_FACTOR = 1</b> is the current shared pool size</p>
-PRO <p><b>SIZE_FACTOR = 2</b> is twice the current shared pool size</p> 
+PRO <p><b>SHARED_POOL_SIZE_FACTOR  = 1</b> is the current shared pool size</p>
+PRO <p><b>SHARED_POOL_SIZE_FACTOR  = 2</b> is twice the current shared pool size</p> 
 PRO <p>Goal should be little to no improvement in <b>ESTD_LC_TIME_SAVED</b> when the shared pool
 PRO size is increased. Shared pool problems are very rare.</p>
 
