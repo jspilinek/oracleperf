2e08cb1afb322e043315ca6869db690b275b6356
jspilinek <101371702+jspilinek@users.noreply.github.com>
2022-12-15 10:04:21 -0600

    highlight.js

--- html/highlight.js
+++ html/highlight.js
@@ -1,3 +1,11 @@
+function highlightRow(rows, rowNum){
+    rows[rowNum].className = 'highlightRow';    
+}
+
+function enableWarning(rows, rowNum, colNum){
+    rows[rowNum].children[colNum].className = 'warn';    
+}
+
 function ImportantParam(){
     var table = document.getElementsByClassName("sortable")[0];
     var rows = table.rows;
@@ -17,63 +25,87 @@ function ImportantParam(){
     }
     
     //Update className
-    for (var i = 1, enableWarning = [false,false]; i < rows.length; i++, enableWarning=[false,false]){
+    for (var i = 1; i < rows.length; i++){
         var textName = rows[i].children[colName].innerText;
         var textValue = rows[i].children[colValue].innerText;
         var textDisplayValue = rows[i].children[colDisplayValue].innerText;
         if (textName == 'optimizer_adaptive_features'){
             if(textValue != 'FALSE'){
-                enableWarning[0] = true;
+                enableWarning(rows,i,colValue);
             }
             if(textDisplayValue != 'FALSE'){
-                enableWarning[1] = true;
+                enableWarning(rows,i,colDisplayValue);
             }
         }
         if (textName == 'optimizer_adaptive_statistics'){
             if(textValue != 'FALSE'){
-                enableWarning[0] = true;
+                enableWarning(rows,i,colValue);
             }
             if(textDisplayValue != 'FALSE'){
-                enableWarning[1] = true;
+                enableWarning(rows,i,colDisplayValue);
             }
         }
         if (textName == 'optimizer_dynamic_sampling'){
             if(textValue != 2){
-                enableWarning[0] = true;
+                enableWarning(rows,i,colValue);
             }
             if(textDisplayValue != 2){
-                enableWarning[1] = true;
+                enableWarning(rows,i,colDisplayValue);
             }
         }
         if (textName == 'optimizer_index_caching'){
             if(textValue != 0){
-                enableWarning[0] = true;
+                enableWarning(rows,i,colValue);
             }
             if(textDisplayValue != 0){
-                enableWarning[1] = true;
+                enableWarning(rows,i,colDisplayValue);
             }
         }
         if (textName == 'optimizer_index_cost_adj'){
             if(textValue != 10){
-                enableWarning[0] = true;
+                enableWarning(rows,i,colValue);
             }
             if(textDisplayValue != 10){
-                enableWarning[1] = true;
+                enableWarning(rows,i,colDisplayValue);
             }
         }
         if (textName == 'optimizer_mode'){
             if(textValue != 'ALL_ROWS'){
-                enableWarning[0] = true;
+                enableWarning(rows,i,colValue);
             }
             if(textDisplayValue != 'ALL_ROWS'){
-                enableWarning[1] = true;
+                enableWarning(rows,i,colDisplayValue);
             }
         }
-        if (enableWarning[0]){
-            rows[i].children[colValue].className = 'warn';
+    }
+}
+
+function NonDefaultParam(){
+    var table = document.getElementsByClassName("sortable")[0];
+    var rows = table.rows;
+    //Find columns
+    var colName = 0;
+    var colValue = 0;
+    var colDisplayValue = 0;
+    for (var i = 0; i < rows[0].childElementCount; i++){
+        columnHeader = rows[0].children[i].innerText;
+        if (columnHeader == 'NAME'){
+            colName = i;
+        }else if (columnHeader == 'VALUE'){
+            colValue = i;
+        }else if (columnHeader == 'DISPLAY_VALUE'){
+            colDisplayValue = i;
         }
-        if (enableWarning[1]){
-            rows[i].children[colDisplayValue].className = 'warn';
+    }
+    
+    //Update className
+    for (var i = 1; i < rows.length; i++){
+        var textName = rows[i].children[colName].innerText;
+
+        console.log("Row: " + textName);
+        if(textName.startsWith("_")){
+            highlightRow(rows,i);
+            console.log("highlight");
         }
     }
 }
@@ -97,11 +129,11 @@ function DbCacheAdvice(){
     for (var i = 1; i < rows.length; i++){
         var cellText = rows[i].children[sizeFactor].innerText;
         if (cellText == 1){
-            rows[i].className = 'active';
+            highlightRow(rows,i);
         }
         cellText = rows[i].children[estdReads].innerText;
         if (cellText > 10){
-            rows[i].children[estdReads].className = 'warn';
+            enableWarning(rows,i,estdReads);
         }
     }
 }
