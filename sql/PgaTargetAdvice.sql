cfb78b3dec787731465b43ed8a43540ec651a82c
jspilinek <101371702+jspilinek@users.noreply.github.com>
2022-12-15 11:33:42 -0600

    Fixed comments

--- sql/PgaTargetAdvice.sql
+++ sql/PgaTargetAdvice.sql
@@ -9,9 +9,9 @@ DEF prevpage = 'DbCacheAdvice.html';
 SET MARK HTML OFF;
 PRO <h2>Comments</h2>
 PRO <p>Predicts how adjusting <b>pga_aggregate_target</b> will impact the PGA usage.</p>
-PRO <p><b>SIZE_FACTOR = 1</b> is the current PGA size</p>
-PRO <p><b>SIZE_FACTOR = 2</b> is twice the current PGA size</p> 
-PRO <p>Goal should be <b>ESTD_PGA_CACHE_HIT_PERCENTAGE > 90</b>. If ESTD_PCT_OF_DB_TIME_FOR_READS
+PRO <p><b>PGA_TARGET_FACTOR = 1</b> is the current PGA size</p>
+PRO <p><b>PGA_TARGET_FACTOR = 2</b> is twice the current PGA size</p> 
+PRO <p>Goal should be <b>ESTD_PGA_CACHE_HIT_PERCENTAGE \&gt; 90</b>. If ESTD_PGA_CACHE_HIT_PERCENTAGE
 PRO is lower, then this is an indication that the PGA is undersized. Typically 1 to 2 GB is large 
 PRO enough for the PGA.</p>
 
