1edfee96e4450526366378cf0b472fe0b53e40b4
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-10-08 08:09:53 -0500

    Order by dbid

--- sql/sqlid/sub/dba_hist_sqlstat.sql
+++ sql/sqlid/sub/dba_hist_sqlstat.sql
@@ -2,4 +2,4 @@ SELECT t.end_interval_time, s.*, t.*
 FROM dba_hist_sqlstat s
 JOIN dba_hist_snapshot t ON t.snap_id = s.snap_id
 WHERE s.sql_id = '&&enteredSQLID'
-ORDER BY s.snap_id DESC;
+ORDER BY s.dbid, s.snap_id DESC;
