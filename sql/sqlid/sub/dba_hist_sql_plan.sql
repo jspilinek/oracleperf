ac058c8db2a1ecaec327f3ee64808fd96905ad24
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-10-09 09:26:48 -0500

    Add date column

--- sql/sqlid/sub/dba_hist_sql_plan.sql
+++ sql/sqlid/sub/dba_hist_sql_plan.sql
@@ -1,4 +1,6 @@
-select *
-from dba_hist_sql_plan
-where sql_id = '&&enteredSQLID'
-order by dbid, plan_hash_value, id ASC;
+SELECT 
+TO_CHAR(p.timestamp, 'YYYY-MM-DD HH24:MI:SS') AS "timestamp", 
+p.*
+FROM dba_hist_sql_plan p
+WHERE p.sql_id = '&&enteredSQLID'
+ORDER BY p.dbid, p.plan_hash_value, p.id ASC;
--- sql/sqlid/sub/dba_hist_sqlstat.sql
+++ sql/sqlid/sub/dba_hist_sqlstat.sql
@@ -1,4 +1,7 @@
-SELECT t.end_interval_time, s.*, t.*
+SELECT 
+TO_CHAR(t.end_interval_time, 'YYYY-MM-DD HH24:MI:SS') AS "end_interval_time", 
+s.*, 
+t.*
 FROM dba_hist_sqlstat s
 JOIN dba_hist_snapshot t ON t.snap_id = s.snap_id
 WHERE s.sql_id = '&&enteredSQLID'
