5f84e44ca003a5760a3b3264162789eb1d2c488f
jspilinek <101371702+jspilinek@users.noreply.github.com>
2025-05-20 09:46:48 -0500

    Active and Blocking SQL

---
+++ sql/sub/ActiveSql-IgnoreQueueLock.sql
@@ -0,0 +1,37 @@
+SELECT LTRIM(TO_CHAR(FLOOR(s.LAST_CALL_ET/86400), '09')) || ' '
+        || LTRIM(TO_CHAR(FLOOR(MOD(s.LAST_CALL_ET,86400)/3600), '09')) || ':'
+        || LTRIM(TO_CHAR(FLOOR(MOD(s.LAST_CALL_ET, 3600)/60), '09')) || ':'
+        || LTRIM(TO_CHAR(MOD(s.LAST_CALL_ET, 60), '09')) AS "ELAPSED_TIME",
+    TO_CHAR(s.SQL_EXEC_START, 'yyyy-mm-dd hh24:mi:ss') AS SQL_EXEC_START,
+    s.INST_ID,
+    s.SID,
+    s.SQL_ID,
+    q.plan_hash_value,
+    q.child_number,
+    s.EVENT,
+    t.piece AS LINE#,
+    REPLACE(t.SQL_TEXT,CHR(10),'') AS SQL_TEXT, 
+    s.WAIT_CLASS,
+    s.SECONDS_IN_WAIT,
+    s.STATE,
+    s.USERNAME,
+    s.MODULE,
+    s.MACHINE,
+    s.blocking_session_status,
+    s.blocking_instance,
+    s.blocking_session
+FROM GV$SESSION s
+JOIN GV$SQL q ON (s.SQL_ID = q.SQL_ID
+    AND s.SQL_ADDRESS    = q.ADDRESS 
+    AND s.SQL_HASH_VALUE = q.HASH_VALUE 
+    AND s.INST_ID = q.INST_ID
+    AND s.SQL_CHILD_NUMBER = q.CHILD_NUMBER)
+JOIN GV$SQLTEXT t ON (s.SQL_ID = t.SQL_ID
+    AND s.SQL_ADDRESS    = t.ADDRESS 
+    AND s.SQL_HASH_VALUE = t.HASH_VALUE 
+    AND s.INST_ID = t.INST_ID)
+WHERE s.STATUS = 'ACTIVE'
+    AND s.USERNAME IS NOT NULL
+    AND s.AUDSID <> USERENV('SESSIONID')
+    AND s.sql_id <> 'g5f60f34546c8' -- Ignore Waiter_SQL: 'SELECT idA2A2 FROM QueueLock WHERE idA2A2=:1  FOR UPDATE'
+ORDER BY ELAPSED_TIME DESC, s.INST_ID, s.SID, t.piece;
--- sql/sub/BlockingSql.sql
+++ sql/sub/BlockingSql.sql
@@ -1,8 +1,10 @@
 select s1.username || '@' || s1.machine || '(SID=' || s1.sid || ' Serial=' || s1.serial# || ' Status=' || s1.status || ')' as blocker,
 s2.username || '@' || s2.machine || ' (SID=' || s2.sid || ' Serial=' || s2.serial# || ' Status=' || s2.status  || ')' as waiter,
-l2.ctime as seconds, s2.wait_class, l2.type as lock_type, sqla1.SQL_FULLTEXT as Blocker_SQL, sqla2.SQL_FULLTEXT as Waiter_SQL, 
+l2.ctime as seconds, s2.wait_class, l2.type as lock_type, 
+sqla1.sql_id as Blocker_SQL_ID, sqla1.SQL_FULLTEXT as Blocker_SQL, 
+sqla2.sql_id as Waiter_SQL_ID,sqla2.SQL_FULLTEXT as Waiter_SQL, 
 s1.program as blocker_program, s1.wait_class as blocker_wait, s1.event as blocker_event,
-sqla3.SQL_FULLTEXT as blocker_prev_SQL
+sqla3.sql_id as Blocker_Prev_SQL_ID, sqla3.SQL_FULLTEXT as blocker_prev_SQL
 from gv$lock l1, gv$session s1, gv$lock l2, gv$session s2, gv$sqlarea sqla1, gv$sqlarea sqla2, gv$sqlarea sqla3
 where s1.sid=l1.sid and s2.sid=l2.sid
 and l1.BLOCK=1 and l2.request > 0
